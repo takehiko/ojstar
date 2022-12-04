711ab62d8243aea054a1977c41f2e63e2c6cfb46
takehiko <takehiko@sys.wakayama-u.ac.jp>
2022-12-05 04:43:47 +0900

    modify database schema and question

--- db/all.sql
+++ db/all.sql
@@ -2,6 +2,7 @@ CREATE TABLE question (
     question_id SERIAL NOT NULL,
     name text UNIQUE,
     question_content text NOT NULL,
+    iframe_url text NOT NULL,
     input text NOT NULL,
     output text NOT NULL,
     input2 text NOT NULL,
@@ -19,6 +20,7 @@ CREATE TABLE submit (
     question_id int NOT NULL,
     result text NOT NULL,
     source text NOT NULL,
+    submitted_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (response_id),
     FOREIGN KEY (question_id) REFERENCES question (question_id)
 );
@@ -26,6 +28,7 @@ INSERT INTO question (
         question_id,
         name,
         question_content,
+        iframe_url,
         input,
         output,
         input2,
@@ -35,12 +38,13 @@ INSERT INTO question (
     )
 VALUES (
         1,
-        '九九',
-        'scanf関数を呼び出して正の整数を受け取り変数nに格納し、出力例（ここではn=4）のように、1×1からn×nまでのかけ算の結果を出力するプログラムを作成してください。<br>同じ行の数値の区切りとして「,」を出力し、行の終わりに「,」を出力してはいけません。<br>また空白文字を出力してはいけません（かけ算の結果を縦方向に揃える必要はありません）。',
-        E'4\n',
-        E'1,2,3,4\n2,4,6,8\n3,6,9,12\n4,8,12,16\n',
-        E'5\n',
-        E'1,2,3,4,5\n2,4,6,8,10\n3,6,9,12,15\n4,8,12,16,20\n5,10,15,20,25\n',
-        E'10\n',
-        E'1,2,3,4,5,6,7,8,9,10\n2,4,6,8,10,12,14,16,18,20\n3,6,9,12,15,18,21,24,27,30\n4,8,12,16,20,24,28,32,36,40\n5,10,15,20,25,30,35,40,45,50\n6,12,18,24,30,36,42,48,54,60\n7,14,21,28,35,42,49,56,63,70\n8,16,24,32,40,48,56,64,72,80\n9,18,27,36,45,54,63,72,81,90\n10,20,30,40,50,60,70,80,90,100\n'
+        '第1問',
+        'scanf関数を呼び出して、2つの正の整数を受け取り変数nおよびmに格納したあと、出力例のように、行数がn、列数がmの「*」による長方形のような並びを出力するプログラムを作成してください。',
+        'https://paiza.io/projects/e/SKlUEYW3t3FKZPTwaLoKSg?theme=twilight',
+        E'3 4\n',
+        E'****\n****\n****\n',
+        E'6 5\n',
+        E'*****\n*****\n*****\n*****\n*****\n*****\n',
+        E'1 1\n',
+        E'*\n'
     );
